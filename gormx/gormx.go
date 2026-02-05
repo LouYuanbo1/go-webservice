2956a2d3b52eb74b3c893014676815cacb0c8d78
LouYuanbo1 <louyuanbo1@gmail.com>
2026-02-05 20:08:18 +0800

    优化了数据库函数，增加更多功能和安全性检查

--- gormx/gormx.go
+++ gormx/gormx.go
@@ -22,9 +22,13 @@ type GormX[T any, ID comparable, PT model.PointerModel[T, ID]] interface {
 	FindByMapFilter(ctx context.Context, filter map[string]any) ([]PT, error)
 	FindByPage(ctx context.Context, page, pageSize int) ([]PT, error)
 	FindByCursor(ctx context.Context, cursor ID, pageSize int) ([]PT, ID, bool, error)
-	Update(ctx context.Context, model PT) error
+	Update(ctx context.Context, updateData PT) error
+	UpdateByStructFilter(ctx context.Context, filter PT, updateData PT) error
+	UpdateByMapFilter(ctx context.Context, filter map[string]any, updateData map[string]any) error
 	DeleteByID(ctx context.Context, id ID) error
 	DeleteByIDs(ctx context.Context, ids []ID) error
+	DeleteByStructFilter(ctx context.Context, filter PT) error
+	DeleteByMapFilter(ctx context.Context, filter map[string]any) error
 }
 
 func NewGormX[T any, ID comparable, PT model.PointerModel[T, ID]](db *gorm.DB) GormX[T, ID, PT] {
