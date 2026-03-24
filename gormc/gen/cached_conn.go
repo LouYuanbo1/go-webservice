9db53b815b359a94c2b487e32fafc25acc62edae
LouYuanbo1 <louyuanbo1@gmail.com>
2026-03-25 00:23:47 +0800

    修改gormc模块,将val作为参数时放在靠前的位置,优化读代码体验

--- gormc/gen/cached_conn.go
+++ gormc/gen/cached_conn.go
@@ -10,14 +10,14 @@ import (
 
 type ExecFn[T any, ID comparable, PT gen.PointerModel[T, ID]] func(ctx context.Context, conn gen.Session[T, ID, PT]) error
 type QueryFn[T any, ID comparable, PT gen.PointerModel[T, ID]] func(ctx context.Context, conn gen.Session[T, ID, PT], val PT) error
-type PrimaryQueryFn[T any, ID comparable, PT gen.PointerModel[T, ID]] func(ctx context.Context, conn gen.Session[T, ID, PT], primaryKey ID, val PT) error
+type PrimaryQueryFn[T any, ID comparable, PT gen.PointerModel[T, ID]] func(ctx context.Context, conn gen.Session[T, ID, PT], val PT, primaryKey ID) error
 
 type CachedConn[T any, ID comparable, PT gen.PointerModel[T, ID]] interface {
 	GetCache(ctx context.Context, key string, val PT) error
 	DelCache(ctx context.Context, key ...string) error
 	Exec(ctx context.Context, exec ExecFn[T, ID, PT], keys ...string) error
-	Query(ctx context.Context, key string, val PT, query QueryFn[T, ID, PT], opts ...gormc.TTLOption) error
-	QueryIndex(ctx context.Context, key string, val PT, keyer func(primary ID) string, indexQuery QueryFn[T, ID, PT], primaryQuery PrimaryQueryFn[T, ID, PT], opts ...gormc.TTLOption) error
+	Query(ctx context.Context, val PT, key string, query QueryFn[T, ID, PT], opts ...gormc.TTLOption) error
+	QueryIndex(ctx context.Context, val PT, key string, keyer func(primary ID) string, indexQuery QueryFn[T, ID, PT], primaryQuery PrimaryQueryFn[T, ID, PT], opts ...gormc.TTLOption) error
 }
 
 type cachedConn[T any, ID comparable, PT gen.PointerModel[T, ID]] struct {
@@ -51,17 +51,17 @@ func (cc *cachedConn[T, ID, PT]) Exec(ctx context.Context, exec ExecFn[T, ID, PT
 	return cc.cache.Del(ctx, keys...)
 }
 
-func (cc *cachedConn[T, ID, PT]) Query(ctx context.Context, key string, val PT, query QueryFn[T, ID, PT], opts ...gormc.TTLOption) error {
-	return cc.cache.Take(ctx, key, val, func(val any) error {
+func (cc *cachedConn[T, ID, PT]) Query(ctx context.Context, val PT, key string, query QueryFn[T, ID, PT], opts ...gormc.TTLOption) error {
+	return cc.cache.Take(ctx, val, key, func(val any) error {
 		return query(ctx, cc.conn, val.(PT))
 	}, gormc.TTLBuilder(cc.cfg.TTL, opts...).GetTTL())
 }
 
 // 可能需要调整过期时间的关系避免缓存击穿或者雪崩
-func (cc *cachedConn[T, ID, PT]) QueryIndex(ctx context.Context, key string, val PT, keyer func(primary ID) string, indexQuery QueryFn[T, ID, PT], primaryQuery PrimaryQueryFn[T, ID, PT], opts ...gormc.TTLOption) error {
+func (cc *cachedConn[T, ID, PT]) QueryIndex(ctx context.Context, val PT, key string, keyer func(primary ID) string, indexQuery QueryFn[T, ID, PT], primaryQuery PrimaryQueryFn[T, ID, PT], opts ...gormc.TTLOption) error {
 	var primaryKey any
 	//查询设置对应的主键
-	if err := cc.cache.Take(ctx, key, primaryKey,
+	if err := cc.cache.Take(ctx, primaryKey, key,
 		func(val any) error {
 			err := indexQuery(ctx, cc.conn, val.(PT))
 			if err != nil {
@@ -79,7 +79,7 @@ func (cc *cachedConn[T, ID, PT]) QueryIndex(ctx context.Context, key string, val
 	}
 
 	//查询主键对应的值
-	return cc.cache.Take(ctx, keyer(primaryKey.(ID)), val, func(val any) error {
-		return primaryQuery(ctx, cc.conn, primaryKey.(ID), val.(PT))
+	return cc.cache.Take(ctx, val, keyer(primaryKey.(ID)), func(val any) error {
+		return primaryQuery(ctx, cc.conn, val.(PT), primaryKey.(ID))
 	}, gormc.TTLBuilder(cc.cfg.TTL, opts...).GetTTL())
 }
